93013ac8c865f22e03c7b3e06c58a4f1b0220afa
warcello <745289603@qq.com>
2017-07-26 11:38:34 +0800

    modified:   app/Http/Controllers/WechatController.php

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -3,19 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Log;
 
 class WechatController extends Controller
 {
+
     public function serve()
     {
     	Log::info('request arrived');
     	$wechat = app('wechat');
-    	$wechat->serve->setMessageHandler(function($message){
+    	$wechat->server->setMessageHandler(function($message){
     		return "欢迎关注柚子养老";
     	});
 
     	Log::info('return response.');
 
-    	return $wechat->serve->serve();
+    	return $wechat->server->serve();
     }
 }
